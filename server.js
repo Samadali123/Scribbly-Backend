cd87654497a425e5d65acfd526914cb58c8d8479
syed samad ali <samadali0125@gmail.com>
2025-05-24 19:52:02 +0530

    remove /

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-   origin : 'https://scribbly-front-end.vercel.app/',
+   origin : 'https://scribbly-front-end.vercel.app',
    credentials: true,
 }));
 
