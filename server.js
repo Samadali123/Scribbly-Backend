504e4e85c2030a74eb715e91c8b9a143347f44f4
syed samad ali <samadali0125@gmail.com>
2025-05-24 21:28:51 +0530

    fix cookies issue

--- server.js
+++ server.js
@@ -22,10 +22,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
 app.use(cors({
-   origin : 'https://scribbly-front-end.vercel.app',
-   credentials: true,
-}));
+  origin: 'https://scribbly-indol.vercel.app',
+  credentials: true,
+}))
+
 
 app.get("/api", (req, res) => {
   res.send("API is working");
